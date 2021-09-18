8be2b5234c03171af07e817e484cf379af2b43d5
Freddie Gar <freddie.gar@outlook.com>
2021-09-18 00:35:26 -0500

    Hide elements using v-show (v-if not render elements)

--- demo/src/components/Demo.vue
+++ demo/src/components/Demo.vue
@@ -1,16 +1,11 @@
 <!-- View -->
 <template>
     <div>
-        <h1>Conditionals</h1>
+        <h1>Hide Elements</h1>
 
-        <p v-if="persons[Math.floor(Math.random() * persons.length)].name !== 'Freddie'">
-            Hello!
-        </p>
-        <p v-else-if="(Math.random() * 10).toFixed() % 3 === 0">
-            You have luck, see you later!
-        </p>
-        <p v-else>
-            Bye!
+        <p>A paragraph is hidden using v-show</p>
+        <p v-show="(Math.random() * 10).toFixed() % 2 === 0">
+            Show paragraph
         </p>
     </div>
 </template>
@@ -21,23 +16,6 @@
         name: 'Demo',
         data () {
             return {
-                persons: [
-                    {
-                        name: "Freddie",
-                        surname: "Gar",
-                        title: "<strike>Developer</strike>"
-                    },
-                    {
-                        name: "Jon",
-                        surname: "Doe",
-                        title: "<strike>QA</strike>"
-                    },
-                    {
-                        name: "Jone",
-                        surname: "Doe",
-                        title: "<strike>Manager</strike>"
-                    }
-                ]
             }
         }
     }
