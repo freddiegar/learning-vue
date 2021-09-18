0f4b17826f3cf24429b68885f1d498e2c76ae68c
Freddie Gar <freddie.gar@outlook.com>
2021-09-18 01:14:17 -0500

    Bind attributes css with Vue using v-bind

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,12 @@
 <template>
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" v-bind:class="{ active: isCurrent }" href="#">Home</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-bind:style="{ backgroundColor: randomColor }">
             <a class="nav-link" href="#">About Me</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" v-bind:style="{ backgroundColor: randomColorAlt, border: 'solid 3px' }">
             <a class="nav-link" href="#">Contact</a>
         </li>
     </ul>
@@ -15,8 +15,12 @@
 <script>
 export default {
     name: 'Nav',
-    setup() {
-
+    data() {
+        return {
+            isCurrent: (Math.random() * 10).toFixed() % 2 === 0,
+            randomColor: '#' + (Math.floor(Math.random() * 6777215)).toString(16),
+            randomColorAlt: '#' + (Math.floor(Math.random() * 6777215)).toString(16)
+        }
     },
 }
 </script>
