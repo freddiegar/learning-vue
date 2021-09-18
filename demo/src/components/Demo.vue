8fd8f91d81afd9cab78417d974bc0b623606ad18
Freddie Gar <freddie.gar@outlook.com>
2021-09-17 22:20:21 -0500

    Using properties of component in template

--- demo/src/components/Demo.vue
+++ demo/src/components/Demo.vue
@@ -1,20 +1,26 @@
+<!-- View -->
 <template>
     <div>
-        <h1>Demo has been loaded!</h1>
+        <h1>Hello {{ person.name }} {{ person.surname }}</h1>
     </div>
 </template>
 
+<!-- Controller -->
 <script>
     export default {
         name: 'Demo',
         data () {
             return {
-
+                person: {
+                    name: "Freddie",
+                    surname: "Gar"
+                }
             }
         }
     }
 </script>
 
+<!-- Styles -->
 <style>
 
 </style>
